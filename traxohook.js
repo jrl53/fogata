b8d61aa00ff14810dc1ba7a41c7c02607b133447
jrl53 <josericardoleonlee@gmail.com>
2016-03-03 12:51:45 +0800

    second

--- traxohook.js
+++ traxohook.js
@@ -2,16 +2,18 @@ var express = require('express');
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
 
-});
+
+
 
 app.post('/parse', function (req, res) {
   console.log("You've got mail!", req);
-  }
+
 });
 
-var server = app.listen(app.get('port'), function() {
-  console.log('Listening on port %d', server.address().port);
+var server = app.listen(port, function() {
+  console.log('Listening on port', port);
 });
+
+console.log("started");
